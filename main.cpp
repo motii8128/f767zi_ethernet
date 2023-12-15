db91a682894ff2a3b171756b10504c1b02810505
motii <108280115+motii8128@users.noreply.github.com>
2023-12-15 17:06:33 +0900

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include "mbed.h"
+#include "handler.hpp"
+#include "interface.hpp"
+#include <cstdint>
+#include <stdint.h>
 
 // main() runs in its own thread in the OS
 int main()
 {
-    while (true) {
+    const char * unity_ip = "127.0.0.1";
+    const uint16_t unity_port = 4000;
 
-    }
+    const char *board_ip = "127.0.0.1";
+    const uint16_t board_port = 5000;
 }
 
