c25d91c14ed7ce32146cbc8365b7de46ca6d40e5
motii <108280115+motii8128@users.noreply.github.com>
2023-12-15 16:31:37 +0900

    Add files via upload

---
+++ interface.hpp
@@ -0,0 +1,41 @@
+#ifndef INTERFACE_HPP_
+#define INTERFACE_HPP_
+
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cwchar>
+#include <vector>
+
+struct wheel_msg{
+    float motor_fl;
+    float motor_fr;
+    float motor_rl;
+    float motor_rr;
+};
+
+struct report_msg{
+    const char message;
+    int code;
+};
+
+template <typename T>
+T deserialize(std::vector<uint8_t>& bytes) {
+    static_assert(std::is_trivially_copyable<T>::value, "Data type is not trivially copyable");
+
+    T data;
+    std::memcpy(&data, bytes.data(), sizeof(data));
+    return data;
+}
+
+template<typename T>
+std::vector<uint8_t> serialize(const T& send_data)
+{
+    std::vector<uint8_t> bytes(sizeof(send_data));
+    std::memcpy(bytes.data(), &send_data, sizeof(send_data));
+    return bytes;
+}
+
+
+#endif
