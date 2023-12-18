301d1d3890e96c9ea393dd17b1e90ae3396982f9
motii <108280115+motii8128@users.noreply.github.com>
2023-12-18 11:41:46 +0900

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -14,18 +14,15 @@ DigitalOut led3(LED3);
 // main() runs in its own thread in the OS
 int main()
 {
-    const char * unity_ip = "127.0.0.1";
+    const char *unity_ip = "127.0.0.1";
     const uint16_t unity_port = 4000;
-
-    const char *board_ip = "127.0.0.11";
-    const uint16_t board_port = 5000;
 
     UDPHandler handler;
     led1 = 0;
     led2 = 0;
     led3 = 0;
 
-    handler.init_net(board_ip, board_port);
+    handler.init_net();
     led1 = 1;
 
     handler.set_destination(unity_ip, unity_port);
@@ -34,7 +31,8 @@ int main()
     while (1) {
         led3 = 0;
         ThisThread::sleep_for(1s);
-        handler.report();
+        const char *data = "Hello, World";
+        handler.pub(data);
         led3 = 1;
         ThisThread::sleep_for(1s);
     }
